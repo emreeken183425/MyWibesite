4f9498c731c530c331b8581e373ee2511031f174
emreeken183425 <emreeken1834@gmail.com>
2022-11-12 23:01:49 +0300

    weather app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 
+import { useState } from 'react';
 import './App.css';
+import Weatherresult from './Weatherresult';
 
 function App() {
+const APP_KEY="1ca93ba8044b4fe688a190436221211"
+let cityinput=""
+const [weatherData, setWeatherData] = useState([])
+
+function cityText(){
+document.querySelector("input").addEventListener("input",(e)=>{
+  e.preventDefault();
+  cityinput=e.target.value;
+  console.log(cityinput);
+})
+}
+async function getdata(value){
+ const data=await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY} &q=${value}&days=3&aqi=no&alerts=no `)
+ const result=await data.json();
+ setWeatherData(result.forecast.forecastday)
+ console.log(result.forecast.forecastday);
+}
   return (
-    <>
-    <nav><h1>LOGO</h1>
-     <ul>
-      <li>Main Page</li>
-      <li>Project</li>
-      <li>About Us</li>
-      <li>Contact</li>
-     </ul>
-    </nav>
-    <section className='backimage' >
-      
-    </section>
-    </>
+   <div>
+    <div className='search' >
+    <input type="text" placeholder="Search a city" onChange={cityText}  />
+    <button onClick={()=> getdata(cityinput)} >Search</button>
+    </div>
+   {weatherData.map(item=>(
+   <Weatherresult
+   key={item.date}
+    data={item.date} 
+    mintemp={item.day.mintemp_c
+    }
+    maxtemp={item.day.maxtemp_c}
+    condition={item.condition.text}
+    icon={item.condition.icon}
+   /> )) }
+   </div>
+
   );
 }
 
 export default App;
+
+
---
+++ src/Weatherresult.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Weatherresult = () => {
+  return (
+    <div className='result' >
+        <h2>Date</h2>
+        <ul>
+            <li><img src="" alt="" />   </li>
+            <li>Weather Status   </li>
+            <li>mintemp C / maxtemp C     </li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Weatherresult
